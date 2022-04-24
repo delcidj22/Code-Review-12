be98c35ad5d375fa8e46157580645a5b501cc4ba
delcidj22 <jdelcid23@gmail.com>
2022-04-23 20:42:58 -0700

    Update with dot net

--- ParksLookUp/Startup.cs
+++ ParksLookUp/Startup.cs
@@ -10,8 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using ParksLookUp.Models;
 using Microsoft.OpenApi.Models;
 
 namespace ParksLookUp
@@ -24,26 +22,29 @@ namespace ParksLookUp
         }
 
         public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddDbContext<StateAndNatParkContext>(opt =>
-                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
-
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ParksLookUp", Version = "v1", Description = "A simple way to look up information on your favorite parks." });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ParksLookUp", Version = "v1" });
             });
         }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParksLookUp  v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParksLookUp v1"));
             }
+
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
