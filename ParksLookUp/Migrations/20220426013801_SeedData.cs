78ee680093947928258be2232da0aa220ad43e99
delcidj22 <jdelcid23@gmail.com>
2022-05-01 00:11:34 -0700

    Add Migrations

---
+++ ParksLookUp/Migrations/20220426013801_SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksLookUp.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    State = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Name", "State" },
+                values: new object[,]
+                {
+                    { 1, "Discovery Park", "Washington" },
+                    { 2, "Yosemite", "California" },
+                    { 3, "Grand Canyon National Park", "Arizona" },
+                    { 4, "Olympic National Park", "Washington" },
+                    { 5, "Fort Stevens", "Oregon" },
+                    { 6, "Letchworth State Park", "New York" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
